2a4b0ec8739173bfa3ea198c76c99c0e000f2ce7
Ted Aronson <ted@mindsnacks.com>
2013-05-06 03:52:12 -0700

    need databases

--- lyricscloud/settings.py
+++ lyricscloud/settings.py
@@ -10,6 +10,10 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+DATABASES = {
+
+}
+
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
