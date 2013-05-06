1d12e9292d3ccaa343a46da55a48cb01abd89ba0
Ted Aronson <ted@mindsnacks.com>
2013-05-06 09:25:02 -0700

    missing import

--- lyricscloud/urls.py
+++ lyricscloud/urls.py
@@ -19,6 +19,8 @@ urlpatterns = patterns('',
 	url(r'^admin/', include(admin.site.urls)),
 )
 
+import settings
+
 if not settings.DEBUG:
 	urlpatterns += patterns('',
 		(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
