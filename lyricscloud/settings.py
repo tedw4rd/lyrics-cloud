8fc85275f40228baf3b5d2675feb80dfd782c444
Ted Aronson <ted@mindsnacks.com>
2013-05-05 23:28:12 -0700

    frontend project

--- lyricscloud/settings.py
+++ lyricscloud/settings.py
@@ -123,6 +123,7 @@ INSTALLED_APPS = (
     'tastypie',
     'datastore',
     'south',
+    'frontend',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
--- lyricscloud/urls.py
+++ lyricscloud/urls.py
@@ -10,7 +10,10 @@ data_api.register(BandResource())
 data_api.register(AlbumResource())
 data_api.register(SongResource())
 
+from frontend.views import index
+
 urlpatterns = patterns('',
+    url(r'^$', index, name='index'),
     url(r'^api/', include(data_api.urls)),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
