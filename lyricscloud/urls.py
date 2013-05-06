9f3675e82cd77cb5028b569ae563c6e0c084c48a
Ted Aronson <ted@mindsnacks.com>
2013-05-06 09:32:51 -0700

    use staticfiles_urlpatterns

--- lyricscloud/urls.py
+++ lyricscloud/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 admin.autodiscover()
 
 from tastypie.api import Api
@@ -19,9 +20,4 @@ urlpatterns = patterns('',
 	url(r'^admin/', include(admin.site.urls)),
 )
 
-import settings
-
-if not settings.DEBUG:
-	urlpatterns += patterns('',
-		(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-	)
+urlpatterns += staticfiles_urlpatterns()
