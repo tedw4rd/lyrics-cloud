483b85794c7619c124daa6ede1d6b3da96da3465
Ted Aronson <ted@mindsnacks.com>
2013-05-05 22:05:00 -0700

    basic models

--- datastore/models.py
+++ datastore/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Band(models.Model):
+	name = models.CharField(max_length=128)
+
+	def __unicode__(self):
+		return unicode(self.name)
+
+
+class Album(models.Model):
+	name = models.CharField(max_length=128)
+	release_date = models.DateField()
+	band = models.ForeignKey(Band)
+
+	def __unicode__(self):
+		return u"%s - %s" % (self.band.name, self.name)
+
+
+class Song(models.Model):
+	name = models.CharField(max_length=128)
+	lyrics = models.TextField()
+	album = models.ForeignKey(Album)
+
+	def __unicode__(self):
+		return u"%s - %s" % (self.album.band.name, self.name)
+
+						
--- lyricscloud/settings.py
+++ lyricscloud/settings.py
@@ -11,12 +11,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'lyricscloud',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
@@ -120,7 +120,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tastypie'
+    'tastypie',
+    'datastore'
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
