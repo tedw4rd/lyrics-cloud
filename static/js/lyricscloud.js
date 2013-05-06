f2886cf5a96fa2ba06199d95cfcda129c573a05f
Ted Aronson <ted@mindsnacks.com>
2013-05-06 02:23:37 -0700

    display word cloud with random sizes

--- static/js/lyricscloud.js
+++ static/js/lyricscloud.js
@@ -1,4 +1,10 @@
 $(function(){
+	var w = 960;
+	var h = 600;
+
+	var g;
+
+	var fill = d3.scale.category20();
 
 	window.SongModel = Backbone.Model.extend({
 		urlRoot: '/api/v1/song/'
@@ -29,21 +35,44 @@ $(function(){
 	});
 
 	window.LyricCloud = Backbone.View.extend({
+
+		draw: function(words){
+			g.selectAll("text")
+					.data(words)
+				.enter().append("text")
+					.style("font-size", function(d) { return d.size + "px"; })
+					.style("font-family", "Impact")
+					.style("fill", function(d, i) { return fill(i); })
+					.attr("text-anchor", "middle")
+					.attr("transform", function(d) {
+						return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+					})
+					.text(function(d) { return d.text; });
+		},
+
 		initialize: function(){
 			this.collection.bind('change', this.render, this);
 			this.collection.bind('add', this.render, this);
 			this.collection.bind('remove', this.render, this);
 			this.collection.bind('reset', this.render, this);
+			g = d3.select("body").append("svg").attr("width", w).attr("height", h).append("g").attr("transform", "translate(" + [w >> 1, h >> 1] + ")");
+
+			this.cloudVis = d3.layout.cloud()
+				.size([w,h])
+				.rotate(function() { return ~~(Math.random() * 2) * 90; })
+				.font("Impact")
+				.fontSize(function(d) { return d.size; })
+				.on("end", this.draw);
 		},
 
 		render: function(eventName){
-			var words = [];
+			this.words = [];
 			for( var i = 0; i < this.collection.total; i++ ){
 				var lyrics = this.collection.models[i].get('lyrics').replace('\n', ' ').replace('\r', ' ');
-				words = words.concat(lyrics.split(' '));
+				this.words = this.words.concat(lyrics.split(' '));
 			}
-
-			alert(words);
+			this.cloudVis.words(this.words.map(function(d) {return {text: d, size: 10 + Math.random() * 90};}));
+			this.cloudVis.start();
 
 			return this;
 		}
